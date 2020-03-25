6ec869759f21b24a036fce185850e053097ad42a
matheusmelchiades <matheusmacielmelchiades@hotmail.com>
2020-03-25 16:41:34 -0300

    Implement method to get data from API weather

--- src/services/api.js
+++ src/services/api.js
@@ -4,4 +4,42 @@ const api = axios.create({
   baseURL: 'https://api.github.com',
 });
 
+function factoryResponseData({name, wind, main, weather, geolocation}) {
+  return {
+    city: name,
+    tempMin: main.temp_min,
+    tempMax: main.temp_max,
+    temperature: main.temp,
+    weatherInfo: weather && weather.length ? weather[0].description : '',
+    humidity: main.humidity,
+    feelsLike: main.feels_like,
+    speed: wind.speed,
+    pressure: main.pressure,
+    geolocation: geolocation,
+  };
+}
+
+export async function getWeatherByGeolocation({lat, lon}) {
+  try {
+    const response = await api.get('weather', {
+      params: {lat, lon},
+    });
+
+    if (response.status !== 200) {
+      throw Error('Error request to api');
+    }
+
+    const data = factoryResponseData({
+      ...response.data,
+      geolocation: {lat, lon},
+    });
+
+    return data;
+  } catch (err) {
+    console.warn('[ERROR - API]', err.message);
+
+    return null;
+  }
+}
+
 export default api;
